23ff118c7577c3effb40e7b4774db69412807fc6
BRUNOMOREAUGARCIASANCHEZ <48569806+BRUNOMOREAUGARCIASANCHEZ@users.noreply.github.com>
2024-02-11 00:09:03 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ btnAnterior=document.getElementById("idAtrasBtn");
 btnPlay=document.getElementById("idPlayBtn");
 btnSub=document.getElementById("idSubBtn");
 btnRandom=document.getElementById("idRandomBtn");
+figure=document.getElementById("idFig");
 
 mainVid=document.getElementById("idVid");
 track=document.getElementById("idSub");
@@ -23,7 +24,7 @@ window.onload = function() {
         document.getElementById('output').textContent = transcript;
 
         console.log(transcript);
-        if(transcript=="reproducir"){
+        if((transcript=="reproducir")||(transcript=="play")||(transcript=="pausa")){
           if(mainVid.paused){
             mainVid.play();
           }else{
@@ -35,10 +36,39 @@ window.onload = function() {
           mainVid.muted = !mainVid.muted;
         }
 
-        if(transcript=="aleatorio"){
-          
+        if((transcript=="aleatorio")||(transcript=="siguiente")||((transcript=="otro"))){
+          x=Math.random()*3;          //GENERA UN NUMERO ENTRE 0 Y 3
+            if(x<=1){
+              mainVid.src=videos[0];
+              track.src=subs[0];
+            }
+            if(x>1&&x<2){
+              mainVid.src=videos[1];
+              track.src=subs[1];
+            }
+            if(x>=2){
+              mainVid.src=videos[2];
+              track.src=subs[2];
+            }
         }
 
+        if((transcript=="adelante")||(transcript=="avanzar")){
+          mainVid.currentTime += 10;
+        }
+        if((transcript=="atras")||(transcript=="retroceder")){
+          mainVid.currentTime -= 10;
+        }
+
+        if((transcript=="subtítulos")||(transcript=="activar subtítulos")||(transcript=="desactivar subtítulos")){
+            var tracks = mainVid.textTracks; // Obtiene todos los tracks de texto (subtítulos)
+             for (var i = 0; i < tracks.length; i++) {
+              var track = tracks[i];
+               if (track.kind === "subtitles") {
+                 track.mode = (track.mode === "showing") ? "disabled" : "showing"; // Alterna entre "showing" y "disabled"
+              }
+          }
+
+        }
 
     };
 
@@ -97,6 +127,59 @@ window.onload = function() {
     btnAnterior.onclick=function(){
       mainVid.currentTime -= 10;
     }
+
+    //HOVER
+    figure.addEventListener('mouseover', function (e) {
+    // Get the target
+    const target = e.target;
+
+    // Get the bounding rectangle of target
+    const rect = target.getBoundingClientRect();
+    console.log(rect.left);
+    console.log(rect.top);
+});
+    figure.addEventListener('mouseout', function() {
+      hover=false;
+      console.log('El mouse ha salido del elemento');
+      mainVid.style.transform = "rotate(0deg)";   //REINICIAR ANIMACION
+    });
+
+    figure.addEventListener('mousemove', function(event) {
+      // Obtener la posición relativa del mouse dentro del elemento
+      var rect = figure.getBoundingClientRect();
+      var x = event.clientX - rect.left; // Posición X relativa
+      var y = event.clientY - rect.top;  // Posición Y relativa
+      //X,Y ESTARAN ENTRE 0 Y EL VALOR DEL ANCHO Y EL ALTO DEL VDEO
+      y=(y-150)/2.5;
+      x=(x-225)/7.5;
+      x=(-1)*x;
+      ang=60
+      //ang=(x*y)/30;
+      //mainVid.style.transform-origin="90% 90%";
+      mainVid.style.transform = "rotate3d("
+      +y+","+x
+      +", 0, "+
+      ang
+      +"deg)";
+      //y=y-150;
+      //x=x-225;
+      //y=y/2.5;
+      //x=x/7.5;
+      //mainVid.style.transform = "rotateX("+y+"deg)";  
+      //mainVid.style.transform = "rotateY("+x+"deg)";
+      // Mostrar la información
+      //mainVid.style.transform = "rotate3d(0, 1, 0, -60deg)";     //HACIA UN LADO
+      //mainVid.style.transform = "rotate3d(1, 0.8, 0, 30deg)";   //HACIA ATRAS
+      //mainVid.style.transform = "rotate3d(1, -1, 0, 45deg)";
+      //mainVid.style.transform = "rotate3d(1, 1, 0, -45deg)";
+      //mainVid.style.transform = "rotate3d(0, 0, 1, 45deg)";   //ROTACION?
+
+
+      console.log(x);
+     console.log(y);
+    });
+
+
 
 }
 
